b3c9dbc009565f7adf85c3d58b8f1404210ce396
Ilias2020 <ilias.dzhumabekov@gmail.com>
2024-08-28 15:48:15 +0600

    add entities

---
+++ src/main/java/my/code/store/entities/TaskEntity.java
@@ -0,0 +1,31 @@
+package my.code.store.entities;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "task")
+public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+
+    @Column(unique = true)
+    String name;
+
+    @Builder.Default
+    Instant createdAt = Instant.now();
+
+    String description;
+
+}
---
+++ src/main/java/my/code/store/entities/TaskStateEntity.java
@@ -0,0 +1,37 @@
+package my.code.store.entities;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "task_state")
+public class TaskStateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+
+    @Column(unique = true)
+    String name;
+
+    Long ordinal;
+
+    @Builder.Default
+    Instant createdAt = Instant.now();
+
+    @Builder.Default
+    @OneToMany
+    List<TaskEntity> tasks = new ArrayList<>();
+
+}
